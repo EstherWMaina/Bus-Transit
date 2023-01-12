ead39c4d4c5464c15ba223385b48c15323317ba8
root <root@KSA-XX-XX07.hq.ksa.go.ke>
2023-01-12 12:45:52 +0300

    controllers

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -4,34 +4,34 @@ class MpesasController < ApplicationController
 
     private
 
-    def generate_access_token_request
-        @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-        @consumer_key = ENV['MPESA_CONSUMER_KEY']
-        @consumer_secret = ENV['MPESA_CONSUMER_SECRET']
-        @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}")
-        headers = {
-            Authorization: "Bearer #{@userpass}"
-        }
-        res = RestClient::Request.execute( url: @url, method: :get, headers: {
-            Authorization: "Basic #{@userpass}"
-        })
-        res
-    end
+        def generate_access_token_request
+            @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+            @consumer_key = ENV['MPESA_CONSUMER_KEY']
+            @consumer_secret = ENV['MPESA_CONSUMER_SECRET']
+            @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}")
+            headers = {
+                Authorization: "Bearer #{@userpass}"
+            }
+            res = RestClient::Request.execute( url: @url, method: :get, headers: {
+                Authorization: "Basic #{@userpass}"
+            })
+            res
+        end
 
-    def get_access_token
-        res = generate_access_token_request()
-        if res.code != 200
-        r = generate_access_token_request()
-        if res.code != 200
-        raise MpesaError('Unable to generate access token')
+        def get_access_token
+            res = generate_access_token_request()
+            if res.code != 200
+            r = generate_access_token_request()
+            if res.code != 200
+            raise MpesaError('Unable to generate access token')
+            end
+            end
+            body = JSON.parse(res, { symbolize_names: true })
+            token = body[:access_token]
+            AccessToken.destroy_all()
+            AccessToken.create!(token: token)
+            token
         end
-        end
-        body = JSON.parse(res, { symbolize_names: true })
-        token = body[:access_token]
-        AccessToken.destroy_all()
-        AccessToken.create!(token: token)
-        token
-    end
 
     def stkpush
         phoneNumber = params[:phoneNumber]
