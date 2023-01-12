70a11d0ba7d938714ce17abd207094d2c072f7a7
root <root@KSA-XX-XX07.hq.ksa.go.ke>
2023-01-12 09:38:05 +0300

    config rails to load environment variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
   config.server_timing = true
   
   #configuring ngrok
-  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts.clear
+  #config.hosts << /[a-z0-9]+\.ngrok\.io/
   
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
