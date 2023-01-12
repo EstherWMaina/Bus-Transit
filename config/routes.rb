4b8410a93ed0dd63d6cd1a43213b8a3e60abb674
root <root@KSA-XX-XX07.hq.ksa.go.ke>
2023-01-12 09:43:56 +0300

    Stk Push route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :mpesas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #stkpush route
+  post 'stkpush', to: 'mpesas#stkpush'
 
   # Defines the root path route ("/")
   # root "articles#index"
